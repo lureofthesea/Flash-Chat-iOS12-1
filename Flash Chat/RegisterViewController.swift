91c413713e96c29c4d0db0bcaf17fd7205f6f807
Zvonko Nikolich <lureofthesea@gmail.com>
2018-09-07 15:00:50 -0400

    complete Lecture 192 (user registration)

--- Flash Chat/RegisterViewController.swift
+++ Flash Chat/RegisterViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
 
@@ -32,7 +32,14 @@ class RegisterViewController: UIViewController {
 
         
         //TODO: Set up a new user on our Firbase database
-        
+        Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
+            if error != nil {
+                print(error!)
+            } else {
+                print("Registration successful!") //success
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+            }
+        }
         
 
         
